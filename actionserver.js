2b9e2cf988f9b6178a106f6ded103c4f7de30a3b
Andrey Nechypurenko <andreynech@gmail.com>
2015-07-24 19:49:09 +0200

    Rework websocket server initialization

--- actionserver.js
+++ actionserver.js
@@ -1,5 +1,18 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: process.env.PORT || 5000});
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
 
 // States and state transition table for simple
 // forward-backward-stop sequence
